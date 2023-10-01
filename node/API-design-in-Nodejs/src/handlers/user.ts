f2bb180489e7745ae7d3ce2c085a8ff713939fdd
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-10-01 18:34:48 +0400

    authentication configure

---
+++ node/API-design-in-Nodejs/src/handlers/user.ts
@@ -0,0 +1,32 @@
+import prisma from "../db";
+import { comparePasswords, createJJWT, hashPassword } from "../modules/auth";
+
+export const createNewUser = async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      username: req.body.username,
+      password: await hashPassword(req.body.password),
+    },
+  });
+
+  const token = createJJWT(user);
+  res.json({ token });
+};
+
+export const signin = async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: req.body.username,
+    },
+  });
+
+  const isValid = await comparePasswords(req.body.password, user.password);
+
+  if (!isValid) {
+    res.status(401);
+    res.json({ message: "not authorized!" });
+  }
+
+  const token = createJJWT(user);
+  res.json({ token });
+};
--- node/API-design-in-Nodejs/src/server.ts
+++ node/API-design-in-Nodejs/src/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
+import { createNewUser, signin } from "./handlers/user";
 
 const app = express();
 const customLogger = (message) => (req, res, next) => {
@@ -20,6 +22,9 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello" });
 });
 
-app.use("/api", router);
+app.use("/api", protect, router);
+
+app.post("/user", createNewUser);
+app.post("/signin", signin);
 
 export default app;
