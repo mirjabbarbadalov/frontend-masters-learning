5d743aa95d65dac1cb7e1401534135ed24e7e46e
Mirjabbar Badalov <91374777+mirjabbarbadalov@users.noreply.github.com>
2023-10-04 19:54:15 +0400

    Update index.ts

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -99,9 +99,7 @@ const veichle = {
  * Union Types
  */
 
-// Union meaning that a variable can be one of the types in the union
-// Union types are denoted with a pipe (|) between the types
-// let x: number | string; ( | ) pipe means or
+
 
 /**
  * Intersection Types
