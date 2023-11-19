aa2623db64b03d1b223f7131f758f7425a515ebc
Mirjabbar Badalov <mirishbedelov08@gmail.com>
2023-11-19 15:23:44 +0400

    Add Iterative sorts

--- intro-to-cs/bigO.js
+++ intro-to-cs/bigO.js
@@ -44,3 +44,80 @@ console.log(makeTuples("Baku"));
  * A loop inside a loop inside a loop would likewise be O(n3).
  * If noo lopps just doing exit or return, then we're doing it in constant time, or O(n)
  */
+
+/**
+ *  Spatial Complexity:
+ * Let's talk about spatial complexity or how much space (e.g. how much RAM or disk space) an algorithm needs to complete.
+ */
+
+/**
+ * Iterative Sorts:
+ *
+ *(1) Bubble Sort:
+ Example:
+     [1, 5, 4, 2, 3]
+
+Are 1 and 5 out of order? No.
+Are 5 and 4 out of order? Yes. Swap.
+
+[1, 4, 5, 2, 3]
+
+Are 5 and 2 out of order? Yes. Swap.
+
+[1, 4, 2, 5, 3]
+
+Are 5 and 3 out of order? Yes. Swap.
+
+[1, 4, 2, 3, 5]
+
+End of the array, did we swap anything? Yes. Loop again.
+Are 1 and 4 out of order? No.
+Are 4 and 2 out of order? Yes. Swap.
+
+[1, 2, 4, 3, 5]
+
+Are 4 and 3 out of order? Yes. Swap.
+
+[1, 2, 3, 4, 5]
+
+Are 4 and 5 out of order? No.
+End of the array, did we swap anything? Yes. Loop again.
+Are 1 and 2 out of order? No.
+Are 2 and 3 out of order? No.
+Are 3 and 4 out of order? No.
+Are 4 and 5 out of order? No.
+End of the array, did we swap anything? No. List is sorted.
+
+ */
+
+//--------------------------------------------------------------------------------------------------------------//
+
+//Example:
+
+/**
+ * 
+function bubbleSort(nums) {
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      // snapshot(nums);
+      if (nums[i] > nums[i + 1]) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  // snapshot(nums);
+}
+
+// unit tests
+// do not modify the below code
+test("bubble sort", function () {
+  const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
+  bubbleSort(nums);
+  expect(nums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+});
+ */
